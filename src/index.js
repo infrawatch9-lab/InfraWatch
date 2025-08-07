3905dc14d130c3c0ebecd31383637bfda090eade
Kelson-D-Pedro <kelsonpedro016@gmail.com>
2025-08-07 17:12:12 +0100

    mudança na rota

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import App from './App';
+import Login from './login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
 
