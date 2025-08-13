4c574b2150f7a66055e3eaf71b2349e11a944119
Melzira Ebo <mebo@c1r5s7.42luanda.com>
2025-08-13 18:27:40 +0100

    indo entendendo

---
+++ src/admin_pages/Dashboards/DashboardWebhooks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function WebhookDashboard() {
+    return (
+      <div >
+        <h1 className="text-2xl font-bold">Settings</h1>
+        <p>Conteúdo das configurações aqui...</p>
+      </div>
+    );
+  }
+  
