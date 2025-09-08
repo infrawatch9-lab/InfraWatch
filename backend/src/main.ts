4173f26fb0dc665d82d037e6053caa0bdc5233d4
leonel_the13 <vicor32leonel@gmail.com>
2025-09-08 12:24:08 +0100

    setting up de cookies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { DynamicAuthGuard } from './auth/dynamic-auth.guard';
 import { JwtService } from '@nestjs/jwt';
-import * as cookieParser from 'cookie-parser';
+const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
